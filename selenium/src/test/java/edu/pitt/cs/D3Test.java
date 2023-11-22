22f045c4401df4fff1259b7542f3a46c046e5c3f
Aakash Arvapalli <aakash.arvapalli@gmail.com>
2023-11-21 21:36:56 -0500

    retry

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -40,6 +40,7 @@ public class D3Test {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options);
+    
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
